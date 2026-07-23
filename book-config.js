055659e61ff3d8c4be17f0a5b57eedb2005d4735
Eric Sanders <u.es170@gmail.com>
2026-07-23 07:50:49 -0700

    Still Here Launch: bio landing page + Amazon book CTA config

--- book-config.js
+++ book-config.js
@@ -1,10 +1,9 @@
 // Broken But Breathing - Amazon link config
-// Ebook preorder live: Kindle ASIN B0H9JYXPZB
-// After July 23, 2026 launch day, change ctaLabel to "BUY ON AMAZON" if you want.
+// Still Here Launch: use the public Amazon listing below for all BUY THE BOOK CTAs.
 
 window.BOOK_CONFIG = {
-  amazonUrl: "https://www.amazon.com/dp/B0H9JYXPZB",
-  ctaLabel: "PREORDER ON AMAZON",
-  placeholderText: "EBOOK PREORDER LIVE NOW",
-  liveBadge: "EBOOK PREORDER LIVE NOW"
+  amazonUrl: "https://www.amazon.com/dp/B0H9G1YHST",
+  ctaLabel: "BUY ON AMAZON",
+  placeholderText: "OUT NOW ON AMAZON",
+  liveBadge: "OUT NOW"
 };
