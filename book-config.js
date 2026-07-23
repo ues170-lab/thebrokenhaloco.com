e029758ac99176121eaa8da16c57c2c7083d7b60
Eric Sanders <u.es170@gmail.com>
2026-07-23 08:13:12 -0700

    Book CTA: replace preorder with BUY NOW (paperback live)

--- book-config.js
+++ book-config.js
@@ -1,9 +1,9 @@
-// Broken But Breathing - Amazon link config
-// Still Here Launch: use the public Amazon listing below for all BUY THE BOOK CTAs.
+﻿// Broken But Breathing - Amazon link config
+// Paperback live.
 
 window.BOOK_CONFIG = {
   amazonUrl: "https://www.amazon.com/dp/B0H9G1YHST",
-  ctaLabel: "BUY ON AMAZON",
-  placeholderText: "OUT NOW ON AMAZON",
-  liveBadge: "OUT NOW"
+  ctaLabel: "BUY NOW",
+  placeholderText: "PAPERBACK OUT NOW",
+  liveBadge: "PAPERBACK OUT NOW"
 };
