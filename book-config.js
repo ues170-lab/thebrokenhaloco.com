d4c757ae817df96d465eb356553ee4809b6589c9
ues170-lab <u.es170@gmail.com>
2026-07-26 18:00:29 -0700

    Add ebook ASIN and wire ebook CTA

--- book-config.js
+++ book-config.js
@@ -1,8 +1,9 @@
 ﻿// Broken But Breathing - Amazon link config
-// Paperback live.
+// Paperback + ebook live.
 
 window.BOOK_CONFIG = {
   amazonUrl: "https://www.amazon.com/dp/B0H9G1YHST",
+  ebookUrl: "https://www.amazon.com/dp/B0H9JYXPZB",
   ctaLabel: "BUY NOW",
   placeholderText: "PAPERBACK OUT NOW",
   liveBadge: "PAPERBACK OUT NOW"
